3f865b3271b455e38d22b5d4e5906a79f3b0cb76
Daniel Whitney <tiedye1@hotmail.com>
2024-02-29 02:42:21 -0500

    add a wheel

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,42 +1,103 @@
-import { ButtonHTMLAttributes, useState } from 'react';
+import { ButtonHTMLAttributes, useEffect, useRef, useState } from 'react';
 
-const Bar = ({ input, output }: { input: number; output: number }) => (
-  <div className="relative h-10 py-1">
-    <div
-      className="h-full overflow-clip transition-all duration-500"
-      style={{
-        clipPath: `rect(auto auto auto auto round 4px)`,
-        width: `${input}%`,
-      }}
-    >
+import { ReactComponent as SteeringWheel } from '../assets/steering-wheel.svg';
+
+const useDefined = <T,>(v: T): T => {
+  const ref = useRef(v);
+  if (v) {
+    ref.current = v;
+  }
+  return ref.current;
+};
+
+const useKeybind = (
+  binding: Partial<
+    Pick<KeyboardEvent, 'key' | 'altKey' | 'shiftKey' | 'ctrlKey' | 'metaKey'>
+  >,
+  action: () => void,
+) => {
+  const { key, altKey, ctrlKey, metaKey, shiftKey } = binding;
+  useEffect(() => {
+    const listener = (event: KeyboardEvent) => {
+      if (
+        (key == null || key === event.key) &&
+        (altKey == null || altKey === event.altKey) &&
+        (ctrlKey == null || ctrlKey === event.ctrlKey) &&
+        (metaKey == null || metaKey === event.metaKey) &&
+        (shiftKey == null || shiftKey === event.shiftKey)
+      ) {
+        action();
+        event.preventDefault();
+      }
+    };
+    document.addEventListener('keydown', listener);
+    return () => document.removeEventListener('keydown', listener);
+  }, [key, altKey, ctrlKey, metaKey, shiftKey, action]);
+};
+
+const Bar = ({
+  inputRange,
+  outputRange,
+  input,
+}: {
+  inputRange: number;
+  outputRange: number;
+  input?: number;
+}) => {
+  const lastInput = useDefined(input);
+  return (
+    <div className="relative h-10 py-1">
       <div
-        className="h-full bg-gradient-to-r from-orange-300 to-red-600 transition-all duration-500"
-        style={{ width: `${10000 / output}%` }}
+        className="h-full overflow-clip transition-all duration-500"
+        style={{
+          clipPath: `rect(auto auto auto auto round 4px)`,
+          width: `${inputRange}%`,
+        }}
       >
-        <svg
-          viewBox="0 0 50 10"
-          preserveAspectRatio="none"
-          className="h-full w-full stroke-black stroke-[4]"
+        <div
+          className="h-full bg-gradient-to-r from-orange-300 to-red-600 transition-all duration-500"
+          style={{ width: `${10000 / outputRange}%` }}
         >
-          {Array(49)
-            .fill(0)
-            .map((_, i) => i + 1)
-            .map((i) => (
-              <line
-                vectorEffect="non-scaling-stroke"
-                strokeLinecap="round"
-                x1={i}
-                x2={i}
-                y1={10}
-                y2={i % 5 === 0 ? 5 : 8}
-                key={i}
-              />
-            ))}
-        </svg>
+          <svg
+            viewBox="0 0 50 10"
+            preserveAspectRatio="none"
+            className="h-full w-full stroke-black stroke-[4]"
+          >
+            {Array(49)
+              .fill(0)
+              .map((_, i) => i + 1)
+              .map((i) => (
+                <line
+                  vectorEffect="non-scaling-stroke"
+                  strokeLinecap="round"
+                  x1={i}
+                  x2={i}
+                  y1={10}
+                  y2={i % 5 === 0 ? 5 : 8}
+                  key={i}
+                />
+              ))}
+          </svg>
+        </div>
       </div>
+      {lastInput != null && (
+        <svg
+          viewBox="0 0 10 10"
+          className="relative h-4 overflow-visible fill-green-600 transition-opacity duration-200"
+          style={{
+            left: `${Math.min(inputRange, lastInput)}%`,
+            opacity: input == null ? '0' : undefined,
+          }}
+        >
+          <polygon
+            points="0,1 4,9 -4,9"
+            className={lastInput > inputRange ? 'fill-red-500' : ''}
+          />
+        </svg>
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 const Key = (props: ButtonHTMLAttributes<HTMLButtonElement>) => (
   <button
@@ -45,18 +106,108 @@ const Key = (props: ButtonHTMLAttributes<HTMLButtonElement>) => (
   />
 );
 
+const Wheel = ({
+  onInput,
+}: {
+  onInput: (value: number | undefined) => void;
+  input: number | undefined;
+}) => {
+  const [{ angle, rotations }, setState] = useState({ angle: 0, rotations: 0 });
+
+  useEffect(() => {
+    if (!angle && !rotations) {
+      onInput(undefined);
+      return;
+    }
+    const normalizedAngle = Math.abs(
+      (angle + 2 * Math.PI * rotations) / Math.PI,
+    );
+    onInput(normalizedAngle * 100);
+  }, [angle, rotations, onInput]);
+
+  const handleSetAngle = (newAngle: number | undefined) => {
+    setState(({ angle, rotations }) => {
+      if (newAngle == null) {
+        onInput(undefined);
+        return { angle: 0, rotations: 0 };
+      }
+      const rotationDelta = (() => {
+        if (newAngle < angle - Math.PI) {
+          return 1;
+        }
+        if (newAngle > angle + Math.PI) {
+          return -1;
+        }
+        return 0;
+      })();
+      return { angle: newAngle, rotations: rotations + rotationDelta };
+    });
+  };
+  return (
+    <SteeringWheel
+      onMouseDown={(e) => {
+        console.log(e);
+        const rect = e.currentTarget.getBoundingClientRect();
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;
+        const initialAngle = Math.atan2(
+          e.clientY - centerY,
+          e.clientX - centerX,
+        );
+        handleSetAngle(0);
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        document.body.style.cursor = 'grabbing';
+
+        const finish = () => {
+          document.body.style.cursor = '';
+
+          controller.abort();
+          handleSetAngle(undefined);
+        };
+        document.addEventListener(
+          'mousemove',
+          (e) => {
+            const currentAngle = Math.atan2(
+              e.clientY - centerY,
+              e.clientX - centerX,
+            );
+            handleSetAngle(currentAngle - initialAngle);
+          },
+          { signal },
+        );
+        document.body.addEventListener('mouseleave', finish, {
+          signal,
+        });
+        document.addEventListener('mouseup', finish, {
+          signal,
+        });
+      }}
+      className="h-24 cursor-grab fill-slate-100 "
+      style={{ transform: `rotate(${angle}rad)` }}
+    />
+  );
+};
+
 const Graph = () => {
   const [actionKey, setActionKey] = useState(20);
+  const [input, setInput] = useState<number>();
+
+  useKeybind({ key: '1' }, () => setActionKey(20));
+  useKeybind({ key: '2' }, () => setActionKey(40));
+  useKeybind({ key: '3' }, () => setActionKey(60));
+  useKeybind({ key: '4' }, () => setActionKey(80));
 
   return (
     <div className="grid gap-3">
       <div className="font-bold">
         Before
-        <Bar input={actionKey} output={actionKey} />
+        <Bar inputRange={actionKey} outputRange={actionKey} input={input} />
       </div>
       <div className="font-bold">
         After
-        <Bar input={100} output={actionKey} />
+        <Bar inputRange={100} outputRange={actionKey} input={input} />
       </div>
       <div className="flex gap-1 py-2">
         <Key onClick={() => setActionKey(20)}>1</Key>
@@ -64,6 +215,7 @@ const Graph = () => {
         <Key onClick={() => setActionKey(60)}>3</Key>
         <Key onClick={() => setActionKey(80)}>4</Key>
       </div>
+      <Wheel onInput={setInput} input={input} />
     </div>
   );
 };
